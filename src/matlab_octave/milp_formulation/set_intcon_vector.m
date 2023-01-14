6f913ed066ea1d200513f460f5d74a0d50bba6c1
tjanus <tomasz.k.janus@gmail.com>
2023-01-15 00:33:40 +0100

    fix(comment): Wrong description in set_intcon_vector.

--- src/matlab_octave/milp_formulation/set_intcon_vector.m
+++ src/matlab_octave/milp_formulation/set_intcon_vector.m
@@ -1,7 +1,6 @@
 function intcon = set_intcon_vector(var_struct)
   % Set intcon vector for the MILP programme formulation.
-  % The vector is composed of a vector of zeros for non-integer variables
-  % followed by a vector of ones for integer variables
+  % The vector is composed of indices of variables that are binary.
   
   % Args:
   %   var_struct - structure of variables with x_cont and x_bin fields
