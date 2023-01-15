b6403b1456336f6e89f8622d4abbd1f5f9e2bd2a
tjanus <tomasz.k.janus@gmail.com>
2023-01-15 02:59:33 +0100

    refactor: Simplify setting LB and UB bounds on decision vars

--- src/matlab_octave/case_studies/two_pump_one_tank/set_constraints_2p1t.m
+++ src/matlab_octave/case_studies/two_pump_one_tank/set_constraints_2p1t.m
@@ -1,14 +1,31 @@
 function y = set_constraints_2p1t(network, tank)
     % Create a constraint structure for the two pump one tank system
-    constraints.ht = [tank.elevation+tank.x_min, tank.elevation+tank.x_max];
-    constraints.hc = [205, 240];
-    constraints.q_pipe = [-80, 80];
-    constraints.q_pump = [0, 80];
-    constraints.p_pump = [0, 10];
-    constraints.s_pump = [0.7, 1.2];
-    constraints.seg_sel = [0, 1];
-    % Make this more generic so that it works with multiple pump (groups) each
-    % one having different numbers of pumps for selection
-    constraints.pump_sel = [0, network.np];
-    y = constraints;
+    % The returned structure must match the fields of the variable
+    % structure used to formulate the MILP problem.
+
+    % Note. Single lower and upper bound is currently supported for each
+    % vector. Later, this and lb and ub boundary setting function should be
+    % extended to allow different boundaries for different network components
+    % e.g. for different tanks or pumps.
+    x_cont = struct();
+    y_cont = struct();
+
+    x_cont.ht = [tank.elevation+tank.x_min, tank.elevation+tank.x_max];
+    x_cont.hc = [205, 240];
+    x_cont.qpipe = [-80, 80];
+    x_cont.ww = [-80, 80];
+    x_cont.ss = [0.7, 1.2];
+    x_cont.qq = [0, 80];
+    x_cont.p = [0, 10];
+    x_cont.q = [0, 80];
+    x_cont.s = [0.7, 1.2];
+
+    x_bin.bb = [0, 1];
+    x_bin.aa = [0, 1];
+    x_bin.n = [0, network.np];
+
+    y.x_cont = x_cont;
+    y.x_bin = x_bin;
 end
+
+
--- src/matlab_octave/milp_formulation/set_variable_bounds.m
+++ src/matlab_octave/milp_formulation/set_variable_bounds.m
@@ -5,104 +5,35 @@ function [lb, ub] = set_variable_bounds(var_struct, constraints)
   % var_struct - structure of variables with x_cont and x_bin fields
   % constarints - 
   
-  % By convention: continuour variables are listed as first, e.g. in variable
-  % vector, followed by binary variables
+  % Create copies of the var stucture. (in this way we can make sure that the
+  % size and order of variables in the lower and upper bound vectors corresponds
+  % to the size and order of variables in the decision variable vector x.
+  lb_struct = var_struct;
+  ub_struct = var_struct;
   
-  [ht_lb, ht_ub] = tank_head_bounds(...
-      var_struct.x_cont, constraints.ht(1), constraints.ht(2));
-  [hc_lb, hc_ub] = calc_head_bounds(...
-      var_struct.x_cont, constraints.hc(1), constraints.hc(2));
-  [qpipe_lb, qpipe_ub] = calc_pipe_flow_bounds(...
-      var_struct.x_cont, constraints.q_pipe(1), constraints.q_pipe(2));
-  [ww_lb, ww_ub] = calc_pipe_seg_flow_bounds(...
-      var_struct.x_cont, constraints.q_pipe(1), constraints.q_pipe(2));
-  [ss_lb, ss_ub] = calc_pump_seg_speed_bounds(...
-      var_struct.x_cont, constraints.q_pipe(1), constraints.q_pipe(2));
-  [qq_lb, qq_ub] = calc_pump_seg_flow_bounds(...
-      var_struct.x_cont, constraints.q_pump(1), constraints.q_pump(2));
-  [p_lb, p_ub] = calc_pump_power_bounds(...
-      var_struct.x_cont, constraints.p_pump(1), constraints.p_pump(2));
-  [s_lb, s_ub] = calc_pump_speed_bounds(...
-      var_struct.x_cont, constraints.s_pump(1), constraints.s_pump(2));
-  [q_lb, q_ub] = calc_pump_flow_bounds(...
-      var_struct.x_cont, constraints.q_pump(1), constraints.q_pump(2));
-  % Set constraints on binary variables
-  [bb_lb, bb_ub] = calc_pipe_seg_sel_bounds(...
-      var_struct.x_bin, constraints.seg_sel(1), constraints.seg_sel(2));
-  [aa_lb, aa_ub] = calc_pump_seg_sel_bounds(...
-      var_struct.x_bin, constraints.seg_sel(1), constraints.seg_sel(2));
-  [n_lb, n_ub] = calc_pump_no_sel_bounds(...
-      var_struct.x_bin, constraints.pump_sel(1), constraints.pump_sel(2));
-      
-  lb = [ht_lb;hc_lb;qpipe_lb;ww_lb;ss_lb;qq_lb;p_lb;s_lb;q_lb;bb_lb;aa_lb;n_lb];
-  ub = [ht_ub;hc_ub;qpipe_ub;ww_ub;ss_ub;qq_ub;p_ub;s_ub;q_ub;bb_ub;aa_ub;n_ub];
-end
-
-%% CONTINUOUS VARIABLES
-% Define inner functions defining lower and upper bounds of all decision
-% variables present in the MILP formulation of the scheduling problem
-function [ht_lb, ht_ub] = tank_head_bounds(vars_cont, ht_min, ht_max)
-  % Assign lower and upper bounds to the vector of tank heads
-  [ht_lb, ht_ub] = constant_bounds(length(vars_cont.ht), ht_min, ht_max);
-end
-
-function [hc_lb, hc_ub] = calc_head_bounds(vars_cont, hc_min, hc_max)
-  % Assign lower and upper bounds to the vector of calculated node heads
-  [hc_lb, hc_ub] = constant_bounds(length(vars_cont.hc), hc_min, hc_max);
-end
-
-function [qpipe_lb, qpipe_ub] = calc_pipe_flow_bounds(...
-    vars_cont, qpipe_min, qpipe_max)
-  % Lower and upper bounds on the vector of pipe flows
-  [qpipe_lb, qpipe_ub] = constant_bounds(...
-    length(vars_cont.qpipe), qpipe_min, qpipe_max);
-end
-
-function [ww_lb, ww_ub] = calc_pipe_seg_flow_bounds(vars_cont, ww_min, ww_max)
-  % Assign lower and upper bounds to the vector of calculated pipe flows in
-  % pipe segments
-  [ww_lb, ww_ub] = constant_bounds(length(vars_cont.ww), ww_min, ww_max);
-end
-
-function [ss_lb, ss_ub] = calc_pump_seg_speed_bounds(vars_cont, ss_min, ss_max)
-  % Lower and upper bounds of the vector of calculated pump speeds in
-  % pump characteristic segments
-  [ss_lb, ss_ub] = constant_bounds(length(vars_cont.ss), ss_min, ss_max);
-end
-
-function [qq_lb, qq_ub] = calc_pump_seg_flow_bounds(vars_cont, qq_min, qq_max)
-  % Lower and upper bounds on the vector of calculated pump flows in
-  % pump characteristic segments
-  [qq_lb, qq_ub] = constant_bounds(length(vars_cont.qq), qq_min, qq_max);
-end
-
-function [p_lb, p_ub] = calc_pump_power_bounds(vars_cont, p_min, p_max)
-  % Lower and upper bounds on pump power consumption
-  [p_lb, p_ub] = constant_bounds(length(vars_cont.p), p_min, p_max);
-end
-
-function [s_lb, s_ub] = calc_pump_speed_bounds(vars_cont, s_min, s_max)
-  % Lower and upper bounds on pump speed
-  [s_lb, s_ub] = constant_bounds(length(vars_cont.s), s_min, s_max);
-end
-
-function [q_lb, q_ub] = calc_pump_flow_bounds(vars_cont, q_min, q_max)
-  % Lower and upper bounds on pump flows
-  [q_lb, q_ub] = constant_bounds(length(vars_cont.q), q_min, q_max);
-end
-
-%% BINARY VARIABLES
-function [bb_lb, bb_ub] = calc_pipe_seg_sel_bounds(vars_bin, bb_min, bb_max)
-  % Lower and upper bounds on pipe segment selection variables
-  [bb_lb, bb_ub] = constant_bounds(length(vars_bin.bb), bb_min, bb_max);
-end
-
-function [aa_lb, aa_ub] = calc_pump_seg_sel_bounds(vars_bin, aa_min, aa_max)
-  % Lower and upper bounds on pump domain selection variables
-  [aa_lb, aa_ub] = constant_bounds(length(vars_bin.aa), aa_min, aa_max);
-end
-
-function [n_lb, n_ub] = calc_pump_no_sel_bounds(vars_bin, n_min, n_max)
-  % Lower and upper bounds on pump selection in the pump group
-  [n_lb, n_ub] = constant_bounds(length(vars_bin.n), n_min, n_max);
+  parsed_fields = {'x_cont', 'x_bin'};
+  
+  % Iterate through all variables
+  field_names = fieldnames(var_struct);
+  for i = 1:length(field_names)
+    var_type = field_names{i};
+    if ~ismember(var_type, parsed_fields)
+      continue
+    end
+    var_names = fieldnames(var_struct.(var_type));
+    for j=1:length(var_names)
+      var_name = var_names{j};
+      constraint = constraints.(var_type).(var_name);
+      % Apply constraints to tensors
+      lb_constraint = apply_constraint(getfield(lb_struct.(var_type), var_name), ...
+          constraint(1));
+      ub_constraint = apply_constraint(getfield(ub_struct.(var_type), var_name), ...
+          constraint(2));
+      % Set constraint values
+    lb_struct.(var_type).(var_name) = lb_constraint;
+    ub_struct.(var_type).(var_name) = ub_constraint;
+    end
+  end
+  lb = struct_to_vector(lb_struct);
+  ub = struct_to_vector(ub_struct);
 end
