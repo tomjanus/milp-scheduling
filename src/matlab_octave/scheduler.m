2a56f3a84fa77beffa90d1f64bfafed1d362b36e
tjanus <tomasz.k.janus@gmail.com>
2023-01-20 01:25:01 +0100

    Retrieve schedules from decision variable vector

--- src/matlab_octave/scheduler.m
+++ src/matlab_octave/scheduler.m
@@ -1,7 +1,8 @@
-function [x1,fval,exitflag]= scheduler(...
-  f_sparse,intcon,A1_sparse,b1_sparse,A1eq_sparse,b1eq_sparse,lb,ub,x0)
+function [n, s]= scheduler(...
+  f_sparse,intcon,A1_sparse,b1_sparse,A1eq_sparse,b1eq_sparse,lb,ub,x0, vars)
   % Wrapper for MATLAB's intlinprog
   % Uses MILP formulation to solve a pump scheduling problem in WDNs
+
   % Args:
   % f_sparse: coefficient vector of the objective
   % intcon: vector of indices of the elements of x that are integer variables
@@ -12,8 +13,33 @@ function [x1,fval,exitflag]= scheduler(...
   % lb: lower bounds on vector x
   % ub: upper bounds on vector x
   % x0: initial point
+
+  % Return
+  % Pump schedules: n, s
   
   [x1,fval,exitflag,output]= intlinprog(...
     f_sparse,intcon,A1_sparse,b1_sparse,A1eq_sparse,b1eq_sparse,lb,ub,x0);
 
+  % Retrieve n and s vectors from x1
+  n = zeros(size(vars.x_bin.n));
+  s = zeros(size(vars.x_cont.s));
+  if size(n) ~= size(s)
+    error("Arrays of active pump numbers n and pump speeds s are not equal");
+  end
+  
+  % Populate n and s arrays with values obtained from intlinprog
+  for i = 1:size(n,1)
+    for j = 1:size(n,2)
+      lin_index_n = lin_index_from_array(vars.x_bin.n, {i,j});
+      lin_index_s = lin_index_from_array(vars.x_cont.s, {i,j});
+      lp_index_n = map_var_index_to_lp_vector(vars, 'n', lin_index_n);
+      lp_index_s = map_var_index_to_lp_vector(vars, 's', lin_index_s);
+      n(i,j) = x1(lp_index_n);
+      s(i,j) = x1(lp_index_s);
+    end
+  end
+  
+  
+  
+  
 end
