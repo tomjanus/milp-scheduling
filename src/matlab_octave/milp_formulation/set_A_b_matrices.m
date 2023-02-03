998282ef31dd8c497002b2681fd5091f3c84d3ec
tjanus <tomasz.k.janus@gmail.com>
2023-02-03 17:37:23 +0100

    Add new inequalities and update scheduling runner

--- src/matlab_octave/milp_formulation/set_A_b_matrices.m
+++ src/matlab_octave/milp_formulation/set_A_b_matrices.m
@@ -1,9 +1,13 @@
-function [A, b] = set_A_b_matrices(vars, linprog, sparse_out)
+function [A, b] = set_A_b_matrices(vars, linprog, lin_power, lin_pipes, pump_groups, sparse_out)
   % Calculate all inequality constraints and create A and b matrices
   [A_p, b_p] = power_ineq_constraint(vars, linprog);
+  [A_p_lin, b_p_lin] = power_model_ineq_constraint(vars, lin_power, linprog);
+  [A_pipe, b_pipe] = pipe_flow_segment_constraints(vars, lin_pipes, linprog);
+  [A_ss_box, b_ss_box] = ss_box_lin_constraints(vars, pump_groups, linprog);
+  [A_qq_box, b_qq_box] = qq_box_lin_constraints(vars, pump_groups, linprog);
   
-  A = [A_p];
-  b = [b_p];
+  A = [A_p; A_p_lin; A_pipe; A_ss_box; A_qq_box];
+  b = [b_p; b_p_lin; b_pipe; b_ss_box; b_qq_box];
   
   
   if (sparse_out == true)
@@ -53,4 +57,236 @@ function [A_p, b_p] = power_ineq_constraint(vars, linprog)
   end
   % Roll out the b vector
   b_p = b_p(:);
+end
+
+function [A_p_lin, b_p_lin] = power_model_ineq_constraint(vars, lin_power, linprog)
+  % Implement the inequality constraints on pump power using the big U trick
+  % - m_s * sj(k) - m_q * qj(k) + pj(k) + nj(k) Upower <= U_power + c (LH constraint)
+  % m_s * sj(k) + m_q * qj(k) + -pj(k) + nj(k) Upower <= Upower -c (RH constraint)
+  % Initialize the left and right hand constraints
+  number_time_steps = size(vars.x_cont.p, 1);
+  number_pumps =size(vars.x_cont.p, 2);
+  A_lh = zeros(number_time_steps*number_pumps,var_struct_length(vars));
+  A_rh = zeros(number_time_steps*number_pumps,var_struct_length(vars));
+  b_lh = zeros(number_time_steps,number_pumps);
+  b_rh = zeros(number_time_steps,number_pumps);
+  
+  m_dq = lin_power.coeff(1);
+  m_ds = lin_power.coeff(2);
+  c = lin_power.coeff(3);
+  
+  % LH inequality
+  row_counter = 1;
+  for j = 1:number_pumps
+    for i = 1:number_time_steps
+      Aineq = vars;
+      Aineq.x_cont.s(i,j) = -m_ds;
+      Aineq.x_cont.q(i,j) = -m_dq;
+      Aineq.x_cont.p(i,j) = 1;
+      Aineq.x_bin.n(i,j) = linprog.Upower;
+      bineq = linprog.Upower + c;
+      A_lh(row_counter,:) = struct_to_vector(Aineq)';
+      b_lh(i,j)=bineq;
+      row_counter = row_counter + 1;  
+    end
+  end
+  % Roll out the b vector
+  b_lh = b_lh(:);
+  row_counter = 1;
+
+  % RH inequality
+  row_counter = 1;
+  for j = 1:number_pumps
+    for i = 1:number_time_steps
+      Aineq = vars;
+      Aineq.x_cont.s(i,j) = m_ds;
+      Aineq.x_cont.q(i,j) = m_dq;
+      Aineq.x_cont.p(i,j) = -1;
+      Aineq.x_bin.n(i,j) = linprog.Upower;
+      bineq = linprog.Upower - c;
+      A_rh(row_counter,:) = struct_to_vector(Aineq)';
+      b_rh(i,j)=bineq;
+      row_counter = row_counter + 1;  
+    end
+  end
+  % Roll out the b vector
+  b_rh = b_rh(:);
+
+  A_p_lin = [A_lh; A_rh];
+  b_p_lin = [b_lh; b_rh];
+end
+
+function [A_pipe, b_pipe] = pipe_flow_segment_constraints(vars, lin_pipes, linprog)
+  % Set the inequality constraints for pipr segment flows
+  % bb(i,k,j) * q*(i-1,j) <= ww(i,k,j) <= bb(i,k,j) * q*(i,j)
+  % where q* flows are the boundary points defining the `domains` of validity
+  % for each segment in the linearized pipe characteristic.
+  % Number of inequalities are equal to n_pipes * n_segments * N * 2 where the
+  % last coefficient 2 describes the fact that we have two inequalities per
+  % equation: the lower bound and the upper bound.
+  
+  % LH inequality:
+  % -wwij(k) + bbij(k) qj(i-1)* <= 0 
+  % RH inequality:
+  % wwij(k) - bbij(k) qj(i)* <= 0 
+  number_time_steps = size(vars.x_cont.ww, 2);
+  number_pipes = size(vars.x_cont.ww, 3);
+  
+  A_lh = zeros(number_time_steps*number_pipes*linprog.NO_PIPE_SEGMENTS,...
+    var_struct_length(vars));
+  A_rh = zeros(number_time_steps*number_pipes*linprog.NO_PIPE_SEGMENTS,...
+    var_struct_length(vars));
+  
+  b_lh = zeros(number_time_steps, number_pipes, linprog.NO_PIPE_SEGMENTS);
+  b_rh = zeros(number_time_steps, number_pipes, linprog.NO_PIPE_SEGMENTS);
+  
+  %LHS inequality
+  row_counter = 1;
+  for i = 1:linprog.NO_PIPE_SEGMENTS
+    for j = 1:number_pipes
+      for k = 1:number_time_steps
+        left_limit = lin_pipes{j}(i).limits{1}(1);
+        Aineq = vars;
+        Aineq.x_cont.ww(i,k,j) = -1;
+        Aineq.x_bin.bb(i,k,j) = left_limit;
+        b_lh(k,j,i) = 0;
+        A_lh(row_counter,:) = struct_to_vector(Aineq)';
+        row_counter = row_counter + 1;
+      end
+    end
+  end
+  b_lh = b_lh(:);
+  
+  %RHS inequality
+  row_counter = 1;
+  for i = 1:linprog.NO_PIPE_SEGMENTS
+    for j = 1:number_pipes
+      for k = 1:number_time_steps
+        right_limit = lin_pipes{j}(i).limits{2}(1);
+        Aineq = vars;
+        Aineq.x_cont.ww(i,k,j) = +1;
+        Aineq.x_bin.bb(i,k,j) = -right_limit;
+        b_rh(k,j,i) = 0;
+        A_rh(row_counter,:) = struct_to_vector(Aineq)';
+        row_counter = row_counter + 1;
+      end
+    end
+  end
+  b_rh = b_rh(:);
+  
+  A_pipe = [A_lh; A_rh];
+  b_pipe = [b_lh; b_rh];
+  
+end
+
+function [A_ss_box, b_ss_box] = ss_box_lin_constraints(vars, pump_groups, linprog)
+  % Binary linearize ss
+  % AAij(k) * smin,j <= ssij(k) <= AAij(k) * smax,j
+  number_pumps = size(vars.x_bin.n, 2);
+  number_time_steps = size(vars.x_bin.n, 1);
+  
+  A_lh = zeros(number_time_steps*number_pumps*linprog.NO_PUMP_SEGMENTS,...
+    var_struct_length(vars));
+  A_rh = zeros(number_time_steps*number_pumps*linprog.NO_PUMP_SEGMENTS,...
+    var_struct_length(vars));
+  b_lh = zeros(number_time_steps, number_pumps, linprog.NO_PUMP_SEGMENTS);
+  b_rh = zeros(number_time_steps, number_pumps, linprog.NO_PUMP_SEGMENTS);
+  
+  %LHS inequality
+  row_counter = 1;
+  for i = 1:linprog.NO_PUMP_SEGMENTS
+    for j = 1:number_pumps
+      
+      % TODO: Modify this to consider multiple pump groups
+      smin = pump_groups(1).pump.smin;
+      
+      for k = 1:number_time_steps
+        Aineq = vars;
+        Aineq.x_cont.ss(i,k,j) = -1;
+        Aineq.x_bin.aa(i,k,j) = smin;
+        b_lh(k,j,i) = 0;
+        A_lh(row_counter,:) = struct_to_vector(Aineq)';
+        row_counter = row_counter + 1;
+      end
+    end
+  end
+  b_lh = b_lh(:);
+
+  %RHS inequality
+  row_counter = 1;
+  for i = 1:linprog.NO_PUMP_SEGMENTS
+    for j = 1:number_pumps
+      
+      % TODO: Modify this to consider multiple pump groups
+      smin = pump_groups(1).pump.smax;
+      
+      for k = 1:number_time_steps
+        Aineq = vars;
+        Aineq.x_cont.ss(i,k,j) = 1;
+        Aineq.x_bin.aa(i,k,j) = -smin;
+        b_rh(k,j,i) = 0;
+        A_rh(row_counter,:) = struct_to_vector(Aineq)';
+        row_counter = row_counter + 1;
+      end
+    end
+  end
+  b_rh = b_rh(:);
+  
+  A_ss_box = [A_lh; A_rh];
+  b_ss_box = [b_lh; b_rh];
+  
+end
+
+function [A_qq_box, b_qq_box] = qq_box_lin_constraints(vars, pump_groups, linprog)
+  % Binary linearize ss
+  % 0 <= qqij(k) <= AAij(k) * qint,jmax
+  number_pumps = size(vars.x_bin.n, 2);
+  number_time_steps = size(vars.x_bin.n, 1);
+  
+  A_lh = zeros(number_time_steps*number_pumps*linprog.NO_PUMP_SEGMENTS,...
+    var_struct_length(vars));
+  A_rh = zeros(number_time_steps*number_pumps*linprog.NO_PUMP_SEGMENTS,...
+    var_struct_length(vars));
+  b_lh = zeros(number_time_steps, number_pumps, linprog.NO_PUMP_SEGMENTS);
+  b_rh = zeros(number_time_steps, number_pumps, linprog.NO_PUMP_SEGMENTS);
+  
+  %LHS inequality
+  row_counter = 1;
+  for i = 1:linprog.NO_PUMP_SEGMENTS
+    for j = 1:number_pumps
+      qmin = 0;
+      for k = 1:number_time_steps
+        Aineq = vars;
+        Aineq.x_cont.qq(i,k,j) = -1;
+        b_lh(k,j,i) = qmin;
+        A_lh(row_counter,:) = struct_to_vector(Aineq)';
+        row_counter = row_counter + 1;
+      end
+    end
+  end
+  b_lh = b_lh(:);
+
+  %RHS inequality
+  row_counter = 1;
+  for i = 1:linprog.NO_PUMP_SEGMENTS
+    for j = 1:number_pumps
+      
+      % TODO: Modify this to consider multiple pump groups
+      qmax = pump_groups(1).pump.qint_smax;
+      
+      for k = 1:number_time_steps
+        Aineq = vars;
+        Aineq.x_cont.ss(i,k,j) = 1;
+        Aineq.x_bin.aa(i,k,j) = -qmax;
+        b_rh(k,j,i) = 0;
+        A_rh(row_counter,:) = struct_to_vector(Aineq)';
+        row_counter = row_counter + 1;
+      end
+    end
+  end
+  b_rh = b_rh(:);
+  
+  A_qq_box = [A_lh; A_rh];
+  b_qq_box = [b_lh; b_rh];
+  
 end
--- src/matlab_octave/run_scheduling.m
+++ src/matlab_octave/run_scheduling.m
@@ -4,27 +4,44 @@ function y = run_scheduling(plot_outputs)
     plot_outputs = false;
   end
   % Step 1 - Load network and input data
-  [input,const,linprog,network,sim] = initialise_2p1t();
+  [input,const,linprog,network,sim,pump_groups] = initialise_2p1t();
   % Step 2 - Simulate the network
   % Calculate flows, calculated heads, power consumption and tank levels
   output = simulator_2p1t(input.init_schedule, network, input, sim);
+  % Find q_op and s_op at 12
+  pump_speed_12 = input.init_schedule.S(12);
+  pump_flow_12 = output.q(2,12)/input.init_schedule.N(12);
+  
   % Step 3 - Visualise simulation results
   if plot_outputs
-    plot_2pt1_simulation_results(output,input.init_schedule.N, ...
-      input.init_schedule.S,input, sim, 1)    
+    plot_2p1t_simulation_results(output,input.init_schedule.N, ...
+      input.init_schedule.S,input, sim, 1);
   end
   % Step 4 - Formulate linear program
   % Initialise continuous and binary variable structures
   vars = initialise_var_structure(network, linprog);
   % Set the vector of indices of binary variables in the vector of decision variables x
   intcon_vector = set_intcon_vector(vars);
-  % Set the vector of objective factors c
+  % Set the vector of objective function coefficients c
   c_vector = set_objective_vector(vars, network, input, linprog, true);
-  % Get variable constraints for the two pump one tank network
+  % Get variable (box) constraints for the two pump one tank network
   constraints = set_constraints_2p1t(network);
   [lb_vector, ub_vector] = set_variable_bounds(vars, constraints);
+  % Linearize the pipe and pump elements
+  lin_pipes = linearize_pipes_2p1t(network, output);
+  lin_pumps = linearize_pumps_2p1t(pump_groups);
+  % Linearize the power consumption model
+  %lin_power = linearize_pump_power_2p1t(pump_groups, pump_flow_12, pump_speed_12);
+  % Linearize pump model with dummy constriants and domain vertices (as not relevant for linearization)
+  constraint_signs = {'>', '<', '<', '>'};
+  domain_vertices = {[0,0], [0,0], [0,0], [0,0]};
+  lin_power = linearize_power_model(pump_groups(1).pump, pump_flow_12, pump_speed_12, ...
+      domain_vertices, constraint_signs);
+  
   % Set the equality constraints parameters Aeq and beq
   
+  y.vars = vars;
+  y.lin_pipes = lin_pipes;
   
 end
 
