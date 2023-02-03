f1b0365009fa9441b045b5c7bd93bf4c01d91084
tjanus <tomasz.k.janus@gmail.com>
2023-02-03 15:12:48 +0100

    Wrapper for  specific for 2p1t system.

---
+++ src/matlab_octave/case_studies/two_pump_one_tank/linerize_pump_power_2pt1.m
@@ -0,0 +1,7 @@
+function out = linearize_pump_power_2pt1(ump_groups, q_op, s_op)
+  % Wrapper for linearize_pumps configured for linearizing the 2pt1
+  % case study model.
+  % Currently, just callse the linearize_pumps model but it's
+  % provided for consistency with linearize_pipes function.
+  out = linearize_power_pumps(pump_groups, q_op, s_op);
+end
