f434218fa07842672aca7224997a9359d09f0548
tjanus <tomasz.k.janus@gmail.com>
2023-03-09 22:35:59 +0100

    fix: Wrong assignment to pump flow variable.

--- src/matlab_octave/milp_formulation/equality_constraints/pumpgroup_constraints.m
+++ src/matlab_octave/milp_formulation/equality_constraints/pumpgroup_constraints.m
@@ -6,7 +6,7 @@ function [Aeq_pumpgroup, beq_pumpgroup] = pumpgroup_constraints(vars, network)
   number_time_steps = size(vars.x_cont.qel, 1);
   Aeq_pumpgroup = zeros(no_pump_groups * number_time_steps, ...
                         var_struct_length(vars));
-  beq_pumpgroup = zeros(no_pump_groups, number_time_steps);
+  beq_pumpgroup = zeros(no_pump_groups, number_time_steps)';
   row_counter = 1;
   pump_flow_index = 1;
   for j = 1:no_pump_groups
@@ -16,12 +16,12 @@ function [Aeq_pumpgroup, beq_pumpgroup] = pumpgroup_constraints(vars, network)
       % Make this code generalize for more pump groups
       npumps = network.pump_groups(j).npumps;
       Aeq.x_cont.qel(i,pump_group_index) = 1;
-      Aeq.q_cont.q(i,pump_flow_index:npumps) = -1; 
-      beq_pumpgroup(j,i)=0;
+      Aeq.x_cont.q(i,pump_flow_index:npumps) = -1; 
+      beq_pumpgroup(i,j)=0;
       Aeq_pumpgroup(row_counter,:) = struct_to_vector(Aeq)';
       row_counter = row_counter + 1;
     end
     pump_flow_index = pump_flow_index + npumps;
   end
-  beq_pumpgroup = beq_pumpgroup(:);
+  beq_pumpgroup = tensor_to_vector(beq_pumpgroup);
 end
