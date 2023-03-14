c9d8458e3943a03a081d808c12c610a6f80c03f0
tjanus <tomasz.k.janus@gmail.com>
2023-03-14 22:58:47 +0100

    feat: Return power and energy cost in the output struct.

--- src/matlab_octave/case_studies/two_pump_one_tank/simulator_2p1t.m
+++ src/matlab_octave/case_studies/two_pump_one_tank/simulator_2p1t.m
@@ -11,6 +11,7 @@ function output = simulator_2p1t(schedule, network, input, sim)
   % q: vector of flows in each pipe for time steps 1 to time horizon, L/S
   % hc: vector of calculated heads for time steps 1 to time horizon, m
   % P: vector of power consumption for time steps 1 to time horizon, ??
+  % f: vector of energy cost for time steps 1 to time_horizon.
   % ht: vector of tank heads for time steps 1 to time horizon, m
   % h: vector of all nodal haeds for time step 1 to time horizon, m
 
@@ -28,7 +29,8 @@ function output = simulator_2p1t(schedule, network, input, sim)
   %   q1 q2 q3 q4 q5  h2  h3  h4  h5
   x0=[0  0  0  30 -30 210 210 232 232];
   P=zeros(sim.TIME_HORIZON,1); % Initialize the power consumption vector
-
+  f=zeros(size(P)); % Initialize the cost vector
+  
   % Run extended period simulation for the period of K steps
   for k=1:sim.TIME_HORIZON
     n=schedule.N(k); % Get number of working pumps at time step k
@@ -44,7 +46,8 @@ function output = simulator_2p1t(schedule, network, input, sim)
     % Get pump consumption from the pump group flow and speed and number of
     % operating pumps at time step k
     P(k) = pump_power_consumption(pump, q(network.pump_group_indices(1),k), ...
-                                  s, n) * input.tariff(k);
+                                  s, n);
+    f(k) = P(k) * input.tariff(k);
     % Advance state in time
     % Calculate water of volume passing through the tank feed pipe
     dV = q(4,k)*time_step;
@@ -57,6 +60,7 @@ function output = simulator_2p1t(schedule, network, input, sim)
   output.q = q;
   output.h = h;
   output.P = P;
+  output.f = f;
   output.hc = hc;
   output.ht = ht;
 
